d5663843134d816d38f03d7dd0454111afe5f2cb
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-21 22:30:48 +0200

    Added basic editor

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,12 +3,16 @@ let Blockly: any = null
 
 console.log(Blockly)
 
-Blockly = require('node-blockly')
+Blockly = require('node-blockly/browser')
 
 console.log(Blockly)
 
 alert("Suc");
 let workspace: any = new Blockly.Workspace()
-let editor: any = Blockly.inject(element, {
-  toolbox: document.getElementById('toolbox')
-})
+try {
+  let editor: any = Blockly.inject(document.getElementById('blocklyDiv'), {
+    toolbox: document.getElementById('toolbox')
+  })
+} catch(e) {
+  console.log(e)
+}
