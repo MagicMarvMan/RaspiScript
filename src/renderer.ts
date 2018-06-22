ea55cb3401a0fe68a9aee03aacb3cff309e26d1a
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-22 07:17:05 +0200

    Improvements

--- src/renderer.ts
+++ src/renderer.ts
@@ -9,10 +9,31 @@ console.log(Blockly)
 
 alert("Suc");
 let workspace: any = new Blockly.Workspace()
-try {
-  let editor: any = Blockly.inject(document.getElementById('blocklyDiv'), {
-    toolbox: document.getElementById('toolbox')
-  })
-} catch(e) {
-  console.log(e)
+let editor: any
+
+editor = Blockly.inject(document.getElementById('blocklyDiv'), {
+  toolbox: document.getElementById('toolbox')
+})
+
+function getCode(editor: any): string {
+  let code: string = Blockly.JavaScript.workspaceToCode(editor);
+  return code;
 }
+
+function runCode(editor: any): boolean {
+  let code: string = getCode(editor)
+  console.log(code)
+  eval(code)
+  return true;
+}
+
+module.exports = {
+  runCode,
+  getCode
+}
+
+document.getElementById('controls').innerHTML = '<button id="button_run">Run</button>';
+
+document.getElementById("button_run").addEventListener("click", function(){
+  runCode(editor);
+});
